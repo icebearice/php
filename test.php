2c0a2ce9ed7b3ac20a676680aabe330c1682b00e
icebearice <iceebarice@qq.com>
2019-07-08 16:12:07 +0800

    modify

--- test.php
+++ test.php
@@ -1,13 +1,15 @@
+<html>
+<body>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    Name: <input type="text" name="fname">
+    <input type="submit">
+</form>
+
 <?php
-echo $_SERVER['PHP_SELF'];
-echo "<br>";
-echo $_SERVER['SERVER_NAME'];
-echo "<br>";
-echo $_SERVER['HTTP_HOST'];
-echo "<br>";
-echo $_SERVER['HTTP_REFERER'];
-echo "<br>";
-echo $_SERVER['HTTP_USER_AGENT'];
-echo "<br>";
-echo $_SERVER['SCRIPT_NAME'];
+$name = $_POST['fname'];
+echo $name;
 ?>
+
+</body>
+</html>
