e1bc70f20a0a6920470fccad990475f55aa9b6fb
icebearice <iceebarice@qq.com>
2019-07-10 18:47:57 +0800

    modify

--- activity_v2/test/test.php
+++ activity_v2/test/test.php
@@ -5,5 +5,5 @@ include dirname(dirname(__FILE__)) . "/include/config.php";
 $bwm = new Car("bwm", 100);
 $audi = new Car("audi", 111);
 
-echo $audi . "\n";
-echo $bwm . "\n";
+echo $audi->getBrand() . "\n";
+echo $bwm->getBrand() . "\n";
