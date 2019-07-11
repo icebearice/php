bff8d27dd440609ca49180c06b9088a2148902e8
icebearice <iceebarice@qq.com>
2019-07-11 10:56:52 +0800

    modify

--- activity_v2/include/config.php
+++ activity_v2/include/config.php
@@ -4,21 +4,20 @@
 if(!defined("CLASS_DIR"))
     define("CLASS_DIR", dirname(dirname(__FILE__)) . "/class/" );
 set_include_path(CLASS_DIR);
-spl_autoload_extensions('.inc,.php,.class.php');
-spl_autoload_register(function ($class){
-    spl_autoload($class);
-});
+//spl_autoload_extensions('.inc,.php,.class.php');
+spl_autoload_register(my_autoload);
 
 
 function my_autoload($classname) {
     echo $classname . "\n";
+    spl_autoload_extensions('.inc,.php,.class.php');
     /**
      * 文件存在的情况下 is_file比file_exists要快N倍
      * 文件不存在的情况下 is_file比file_exists要慢
      * 我们这种基本存在的啦
      */
     if(is_file($classname)){
-        require_once ($classname);
+        spl_autoload ($classname);
     }   else {
         throw new Exception('class file' . $classname . 'not found');
     }
